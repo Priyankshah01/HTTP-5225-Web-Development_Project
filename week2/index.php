1c30c3930eb488442fe25405b0ca6b1ce2e42ec6
priyank shah <priyankshah0101@gmail.com>
2025-05-14 16:37:36 -0400

    week 2 update

---
+++ week2/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Hello World!</h1>
+
+    <!-- learning echo -->
+
+    <?php
+
+        echo "<h1>Hello World!</h1>";
+
+        echo "<p> My Name is Priyank Shah";
+
+    ?>
+
+    <hr>
+
+    <!-- learning Variables -->
+
+    <?php
+
+    $fname = "Priyank";
+    $lname = "Shah";
+
+    echo "<h1> Hello  $fname  $lname </h1>";
+
+    ?>
+</body>
+</html>
